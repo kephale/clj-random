21e3dd4168fa901bfb1b7fe9a96e2e2377b601e4
Kyle IS Harrington <kyle.i.s.harrington@gmail.com>
2019-04-27 09:10:42 -0400

    Fix groupId

--- project.clj
+++ project.clj
@@ -1,10 +1,9 @@
-(defproject clj-random "0.1.8-SNAPSHOT"
+(defproject com.kephale/clj-random "0.1.8-SNAPSHOT"
   :description "A library for random number generators in Clojure."
   :url "https://github.com/kephale/clj-random"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :java-source-paths ["java"]
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/math.numeric-tower "0.0.2"]
-                 [kephale/uncommons-maths "1.2.3"]
-                 ])
+                 [kephale/uncommons-maths "1.2.3"]])
